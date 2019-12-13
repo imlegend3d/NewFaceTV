b392354c90e84c6906d1a16a172904a985b1db49
David <johandgonzalez@hotmail.com>
2019-12-13 18:25:40 -0500

    Initial Commit

---
+++ NewFaceTV/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewFaceTV
+//
+//  Created by David on 2019-12-13.
+//  Copyright © 2019 David. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
